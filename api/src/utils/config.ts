51d4549aa5d7a00dd98ee3a9d1774bdb474af59d
Sam Foster <sfoster@brandwatch.com>
2020-06-16 21:55:23 +0100

    Updating configs for heroku

--- api/src/utils/config.ts
+++ api/src/utils/config.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 let MONGODB_URI = process.env.MONGODB_URI
-const PORT: string = process.env.PORT || '9000'
+let PORT: string = process.env.PORT || '9000'
 
 if (process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'development') {
   MONGODB_URI = process.env.TEST_MONGODB_URI
